2649536f30f90747a1598a14c3200b442bf92437
Davi <davimachadocruz@gmail.com>
2024-03-29 18:29:31 -0300

    Atualizações de projeto no Portfolio

--- lib/projectsData.ts
+++ lib/projectsData.ts
@@ -12,6 +12,18 @@ const projectsData = [
     git: "https://github.com/davimcruz/Coldfy-Project",
   },
   {
+    name: "SpaceLabs",
+    description: "Marketplace Completo para Gamers",
+    image: "https://davimachado.cloud/spacelabs.svg",
+    text: `Solução Web feita inteiramente para Gamers. Esse projeto conta com um sistema inteiro de Dashboard, Perfil, Pedidos e tudo o que uma boa dashboard tem direito.
+    Solucionamos uma grande dor da comunidade: comprar e vender serviços dentro do Minecraft. Totalmente otimizado para escala e experiência de usuário. `,
+    status: "Online",
+    tech: ["React.js", "Next.js", "Node.js"],
+    others: ["Motion", "International App", "Vercel Hosted"],
+    link: "https://spacelabs.vercel.app/dashboard/overview",
+    git: "https://github.com/davimcruz/SpaceLabs-Frontend",
+  },
+  {
     name: "Paga Muito",
     description: "Salas de Apostas para Cassinos",
     image: "https://davimachado.cloud/pagamuito.svg",
@@ -28,12 +40,11 @@ const projectsData = [
     name: "SpaceLabs",
     description: "Marketplace Completo para Gamers",
     image: "https://davimachado.cloud/spacelabs.svg",
-    text: `Solução Web desenvolvida para solucionar a maior dor do mercado de cassinos: aquisição de leads. Desenvolvida inteiramente para PWA e se portar na web
-    como um aplicativo nativo. Pela necessidade do cliente, utilizamos iFrames múltiplos e alternados totalmente otimizados para desktop e mobile.
-    Também optamos por integrações com as API's das respectivas casas.`,
+    text: `Solução Web feita inteiramente para Gamers. Esse projeto conta com um sistema inteiro de Dashboard, Perfil, Pedidos e tudo o que uma boa dashboard tem direito.
+    Solucionamos uma grande dor da comunidade: comprar e vender serviços dentro do Minecraft. Totalmente otimizado para escala e experiência de usuário. `,
     status: "Online",
     tech: ["React.js", "Next.js", "Node.js"],
-    others: ["Optimized iFrames", "PWA", "Vercel Hosted"],
+    others: ["Motion", "International App", "Vercel Hosted"],
     link: "https://spacelabs.vercel.app/dashboard/overview",
     git: "https://github.com/davimcruz/SpaceLabs-Frontend",
   },
